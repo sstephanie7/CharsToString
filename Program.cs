b4711d3bebd9a3afc01764e9d05e5c4d6e53fadf
Stephanie Tahova <106015292+sstephanie7@users.noreply.github.com>
2022-05-31 17:32:36 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CharsToString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char one = char.Parse(Console.ReadLine());
+            char two = char.Parse(Console.ReadLine());
+            char three = char.Parse(Console.ReadLine());
+
+            string str = "";
+            str = str + one + two + three;
+
+            Console.WriteLine(str);
+        }
+    }
+}
